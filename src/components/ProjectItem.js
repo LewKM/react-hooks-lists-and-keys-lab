05e55884a8110056d15a7b0501e2f2c88ca9d722
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-07-14 15:18:13 +0300

    Done

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React from 'react';
 
 function ProjectItem({ name, about, technologies }) {
-  return (
-    <div className="project-item">
-      <h3>{name}</h3>
-      <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
-    </div>
-  );
-}
+    const techSpanElement = technologies.map((technology) => < span key = { technology } > { technology } < /span>);
+        return ( <
+            div className = "project-item" >
+            <
+            h3 > { name } < /h3> <
+            p > { about } < /p>
 
-export default ProjectItem;
+            <
+            div className = "technologies" > { techSpanElement } < /div> <
+            /div>
+        );
+    }
+
+    export default ProjectItem;
